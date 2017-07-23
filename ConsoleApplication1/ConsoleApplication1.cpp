175e87d4fb8bcf7e1185976265390e3fc04a3eb0
nora inu <ikehata.seijiro@tres-bind.co.jp>
2017-07-23 21:41:38 +0900

    first

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,66 @@
+// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#include <gst/gst.h>
+
+int main(int argc, char *argv[])
+{
+	printf("aaaaa\n");
+
+	GString *launch_str;
+	GstElement *pipeline;
+	GstBus *bus;
+	GstMessage *msg;
+
+	gst_init(&argc, &argv);
+
+	launch_str = g_string_new(NULL);
+//	g_string_printf(launch_str,
+//		"playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
+	g_string_printf(launch_str,
+		"filesrc location=C:/tmp/a.mp4 ! decodebin ! videoconvert ! autovideosink", NULL);
+
+	pipeline = gst_parse_launch(launch_str->str, NULL);
+
+	if (pipeline == NULL) {
+		g_print("parse_launch fail.\n");
+		return 1;
+	}
+
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+	bus = gst_element_get_bus(pipeline);
+	gint handle_msg;
+	handle_msg = GST_MESSAGE_EOS | GST_MESSAGE_ERROR;
+	msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)handle_msg);
+
+	GError *err;
+	gchar *debug_info;
+	if (msg != NULL)
+		switch (GST_MESSAGE_TYPE(msg)) {
+		case GST_MESSAGE_ERROR:
+			gst_message_parse_error(msg, &err, &debug_info);
+			g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+			g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+			g_clear_error(&err);
+			g_free(debug_info);
+			break;
+		case GST_MESSAGE_EOS:
+			g_print("End-Of-Stream reached.\n");
+			break;
+		default:
+			/* We should not reach here because we only asked for ERRORs and EOS */
+			g_printerr("Unexpected message received.\n");
+			break;
+		}
+	gst_object_unref(bus);
+	gst_element_set_state(pipeline, GST_STATE_NULL);
+	gst_object_unref(pipeline);
+
+	while (true) {
+
+	}
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Gstreamer事始め
+1.VirtualBoxのWindowsではうまくいかない。  
+2.Macもいまいち。  
+3.Linuxが本拠地っぽいけど試していない。
+4.TeamViewer経由でのWin7でそれなりに使えた。
+
+# 死にそうになった
+土曜のよるからやって20時間ぐらい格闘した。  
+Cの勉強は全くできなかった。畜生  
+
+# 成果
+なんとなくgstreamerがわかってきたのではないでしょうか？
+まとめられないけれど。
+実際のリアルな要求がどんなものなのか不明なのでちょっとよくわかりませんね。
+
+# スクリーンショットの時間化感覚
+
+    powershell -Command "Measure-Command {.\ConsoleApplication2.exe}"
+    Days              : 0
+    Hours             : 0
+    Minutes           : 0
+    Seconds           : 0
+    Milliseconds      : 244
+    Ticks             : 2447313
+    TotalDays         : 2.83253819444444E-06
+    TotalHours        : 6.79809166666667E-05 
+    TotalMinutes      : 0.004078855
+    TotalSeconds      : 0.2447313
+    TotalMilliseconds : 244.7313
+
+まったくもって話にならない程度には遅い。
+このあたり、既存の実装がどういう構造になっているのか、どういう工夫をしているのか知りたいところ。
+
+# 実際問題
+なにをするのかイマイチはっきりつかめていない。
